d0770fbafed9fa43e5e91edf762a262742f5f34f
Jay Jay Ting <tingjayj@gmail.com>
2019-06-04 22:08:52 +0800

    adding first c file

---
+++ C++/CPPWorkspace/Test1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	int favorite_number;
+	std::cout<< "write your favorite number btw 1-100";
+	std::cin>> favorite_number;
+	std::cout<< "amazing... " << favorite_number << " is my favorite number too!"<< std::endl;
+	return 0;
+}
